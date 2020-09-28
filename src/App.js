779f97218bc5dd7db1005e647718936fb591569f
newt0 <py_lambda@yahoo.co.jp>
2020-09-28 17:10:56 +0900

    add: useStyles, classes

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
 import { Button, Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 import React from "react";
 import "./App.css";
 
+const useStyles = makeStyles({
+  helloThereStyles: {
+    fontStyle: "oblique",
+    color: "red",
+    fontSize: "30px",
+  },
+  buttonStyles: {
+    color: "green",
+    border: "0",
+  },
+});
+
 function App() {
+  const classes = useStyles();
+
   return (
     <div className="app">
-      <Typography variant="h2" color="primary">
-        h2
+      <Typography className={classes.helloThereStyles}>
+        helloThereStyles
       </Typography>
-      <h2>h2</h2>
 
-      <Button color="secondary" variant="outlined">
-        first button
-      </Button>
-      <Button variant="outlined" fullWidth>
-        fullWidth Button
-      </Button>
-      <Button
-        variant="text"
-        href="https://material-ui.com/ja/api/button/"
-        target="__blank"
-      >
-        API page
-      </Button>
+      <Button className={classes.buttonStyles} variant="outlined">classes.buttonStyles</Button>
     </div>
   );
 }
