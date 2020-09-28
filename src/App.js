519d8296119b26c3f4a2074eae32d1c365a0f6f8
newt0 <py_lambda@yahoo.co.jp>
2020-09-28 23:34:25 +0900

    add: Grid

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import { Button, Typography } from "@material-ui/core";
-import { makeStyles } from "@material-ui/core/styles";
 import React from "react";
 import "./App.css";
+import { Grid } from "@material-ui/core";
+import Header from "./Header";
 
 function App() {
-  const classes = useStyles();
-
-  return <div className="app"></div>;
+  return (
+    <Grid container direction="column">
+      <Grid item>
+        <Header />
+      </Grid>
+      <Grid item container>
+        <Grid item xs={0} sm={2} />
+        <Grid item xs={12} sm={8}>
+          ContentContentContentContentContentContentContentContentContentContent
+          ContentContentContentContentContentContentContentContentContentContent
+          ContentContentContentContentContentContentContentContentContentContent
+          ContentContentContentContentContentContentContentContentContentContent
+        </Grid>
+        <Grid item xs={0} sm={2} />
+      </Grid>
+    </Grid>
+  );
 }
 
 export default App;
