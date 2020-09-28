25cbee157df3f689d7043a119ab298569457dffc
newt0 <py_lambda@yahoo.co.jp>
2020-09-28 16:50:47 +0900

    add: Component's props(ex. Button)

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
+import { Button } from "@material-ui/core";
 import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="app">test</div>;
+  return (
+    <div className="app">
+      <Button color="secondary" variant="outlined">first button</Button>
+      <Button variant="outlined" fullWidth>fullWidth Button</Button>
+      <Button variant="text" href="https://material-ui.com/ja/api/button/" target="__blank">API page</Button>
+    </div>
+  );
 }
 
 export default App;
