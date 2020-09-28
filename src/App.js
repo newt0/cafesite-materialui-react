1efdac4e1eb73ae4e13cbabbf5af1ee3887db774
newt0 <py_lambda@yahoo.co.jp>
2020-09-28 17:02:43 +0900

    add: apply ow theme by ThemeProvider

--- src/App.js
+++ src/App.js
@@ -5,9 +5,19 @@ import "./App.css";
 function App() {
   return (
     <div className="app">
-      <Button color="secondary" variant="outlined">first button</Button>
-      <Button variant="outlined" fullWidth>fullWidth Button</Button>
-      <Button variant="text" href="https://material-ui.com/ja/api/button/" target="__blank">API page</Button>
+      <Button color="secondary" variant="outlined">
+        first button
+      </Button>
+      <Button variant="outlined" fullWidth>
+        fullWidth Button
+      </Button>
+      <Button
+        variant="text"
+        href="https://material-ui.com/ja/api/button/"
+        target="__blank"
+      >
+        API page
+      </Button>
     </div>
   );
 }
