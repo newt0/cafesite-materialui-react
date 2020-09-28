4538ac2b71b631daa1434ccd1ea28f4572650239
newt0 <py_lambda@yahoo.co.jp>
2020-09-28 23:46:54 +0900

    add: makeStyles in the Header

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,20 @@
-import { AppBar, Toolbar, Typography } from "@material-ui/core";
+import { AppBar, makeStyles, Toolbar, Typography } from "@material-ui/core";
 import React from "react";
 
+const useStyles = makeStyles(() => ({
+  typographyStyles: {
+    flex: 1,
+  },
+}));
 function Header() {
+  const classes = useStyles();
+
   return (
     <AppBar position="static">
       <Toolbar>
-        <Typography>HeaderHeader</Typography>
+        <Typography className={classes.typographyStyles}>
+          HeaderHeader
+        </Typography>
       </Toolbar>
     </AppBar>
   );
