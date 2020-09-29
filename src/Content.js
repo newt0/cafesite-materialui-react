1805d98218f409889809ede71a7b1c0f6331c999
newt0 <py_lambda@yahoo.co.jp>
2020-09-29 14:02:54 +0900

    add: Grid in the Content

--- src/Content.js
+++ src/Content.js
@@ -1,10 +1,21 @@
+import { Grid } from "@material-ui/core";
 import React from "react";
 import CoffeeCard from "./CoffeeCard";
 
 function Content() {
   return (
     <div>
-      <CoffeeCard />
+      <Grid container>
+        <Grid item xs={4}>
+          <CoffeeCard />
+        </Grid>
+        <Grid item xs={4}>
+          <CoffeeCard />
+        </Grid>
+        <Grid item xs={4}>
+          <CoffeeCard />
+        </Grid>
+      </Grid>
     </div>
   );
 }
