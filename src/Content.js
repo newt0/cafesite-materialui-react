f6c739ed49a4f233455e41fb6ff6cb77c3d140b8
newt0 <py_lambda@yahoo.co.jp>
2020-09-29 14:52:27 +0900

    add: getCoffeeMakerCard() in Content

--- src/Content.js
+++ src/Content.js
@@ -1,19 +1,20 @@
 import { Grid } from "@material-ui/core";
 import React from "react";
 import CoffeeCard from "./CoffeeCard";
+import coffeeMakeList from "./constant";
 
 function Content() {
+  const getCoffeeMakerCard = (coffeeMakerObj) => {
+    // const { title, price, description, imageUrl, avatarUrl } = coffeeMakerObj;
+    return <CoffeeCard {...coffeeMakerObj} />;
+  };
   return (
     <div>
       <Grid container spacing={4}>
         <Grid item xs={12} sm={4}>
-          <CoffeeCard
-            title="Hamilton Beach Flexbrew"
-            subtitle="$89.99"
-            avatarSrc="https://images-na.ssl-images-amazon.com/images/I/81mjh5ZSu1L._AC_SL1500_.jpg"
-            description="No two coffee drinkers are the same – so, the flexibility to customize how and what you brew is key. From fresh grounds to K-Cup pods, the Hamilton Beach FlexBrew 2-Way Coffee Maker gives you the options to please fans of all types of coffees."
-            imageUrl="https://images-na.ssl-images-amazon.com/images/I/818rz3CIaFL._AC_SL1500_.jpg"
-          />
+          {coffeeMakeList.map((coffeeMakerObj) =>
+            getCoffeeMakerCard(coffeeMakerObj)
+          )}
         </Grid>
       </Grid>
     </div>
